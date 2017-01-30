49d1fa24ae9853516de1425c2b4845e0ca20150e
T.R.Rohith <trrohith1@gmail.com>
2017-01-30 19:09:52 +0530

    Initial Commit
    Automatically monitors for PDF files in the directory it is placed and
    prints them using the default printer.

---
+++ Automatically Print/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Automatically_Print
+{
+    public partial class Form1 : Form
+    {
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        ListBox listbox;
+        FileSystemWatcher watcher;
+        String location;
+
+        public Form1()
+        {
+            InitializeComponent();
+            listbox = listBox1;
+            location = AppDomain.CurrentDomain.BaseDirectory;
+            watch();
+        }
+
+        public void printThis(String FileName)
+        {
+            ProcessStartInfo info = new ProcessStartInfo(FileName.Trim());
+            info.Verb = "Print";
+            info.CreateNoWindow = true;
+            info.WindowStyle = ProcessWindowStyle.Hidden;
+            Process.Start(info);
+        }
+
+        private void watch()
+        {
+            watcher = new FileSystemWatcher();
+            watcher.Path = location;
+            watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite
+                                   | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+            watcher.Filter = "*.pdf";
+            watcher.Created += new FileSystemEventHandler(OnCreated);
+            watcher.IncludeSubdirectories = false;
+            watcher.EnableRaisingEvents = true;
+        }
+
+        public void OnCreated(object sender, FileSystemEventArgs e)
+        {
+            this.Invoke((MethodInvoker)delegate
+            {
+                listbox.Items.Add(e.Name);
+                printThis(e.FullPath);
+            });
+        }
+        
+    }
+}
